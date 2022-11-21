89241bfd5da248a6c0faea088beda3e07029151d
Julia Lima <julia.manvailer17@icloud.com>
2022-11-21 16:56:11 -0500

    displayed student info

--- student-tracker/src/Pages/OneStudent.js
+++ student-tracker/src/Pages/OneStudent.js
@@ -1,5 +1,38 @@
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
 const OneStudent = () => {
-  return <h1>hello</h1>
+  let { id } = useParams()
+  const [studentInfo, setStudentInfo] = useState({})
+  const [studentClasses, setStudentClasses] = useState([])
+
+  const getStudentInfo = async () => {
+    let res = await axios.get(`${BASE_URL}/student/${id}`)
+    setStudentInfo(res.data)
+    setStudentClasses(res.data.classes)
+  }
+
+  useEffect(() => {
+    getStudentInfo()
+  }, [])
+
+  return (
+    <div>
+      <h1>{studentInfo.name}</h1>
+      <h3>{studentInfo.email}</h3>
+      <div>
+        {studentClasses.map((element) => (
+          <div>
+            <h4>{element.name}</h4>
+            <h4>{element.Grades[0].letter}</h4>
+            <h4>{element.Grades[0].score}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default OneStudent
--- student-tracker/src/components/StudentList.js
+++ student-tracker/src/components/StudentList.js
@@ -5,11 +5,16 @@ import { useState, useEffect } from 'react'
 
 const StudentList = ({ getOneStudent }) => {
   const [students, setStudents] = useState([])
+  const [viewForm, setViewForm] = useState(false)
 
   const getAllStudents = async () => {
     const res = await axios.get(`${BASE_URL}/student/all`)
     console.log(res.data)
     setStudents(res.data)
+  }
+
+  const toggleForm = () => {
+    setViewForm(true)
   }
 
   useEffect(() => {
@@ -18,6 +23,8 @@ const StudentList = ({ getOneStudent }) => {
 
   return (
     <div>
+      <button onClick={toggleForm}>Add Student</button>
+      {viewForm ? <div></div> : null}
       {students.map((student) => (
         <StudentCard
           key={student.id}
